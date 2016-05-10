42ccc72c8c9855e888211e7ea57b911c4930cb9f
Mateusz Ostafil <ratwolf0@gmail.com>
2016-05-10 14:33:13 +0000

    Add message router

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/webhook', function (req, res) {
         if(event.message && event.message.text){
             var text = event.message.text;
             console.log('Received: ' + text + ' from: ' + senderId);
-            echo(senderId, text);
+            messageRouter(senderId, text);
         }
     }
     
@@ -45,10 +45,30 @@ app.listen(app.get('port'), function () {
     console.log('Magic starts on port', app.get('port'));
 });
 
+function messageRouter(senderId, text){
+    var msg = '';
+    if(text.indexOf("info") > 0){
+        //informacje o bocie
+        msg = "Witaj! \n Jestem tutaj żeby Ci pomóc. Wpisz: 'Chciałbym zgłosić barierę', 'Potrzebuję pomocy', 'Mam pytanie' lub cokolwiek innego.";
+    } else if(text.indexOf("barier") > 0){
+        //prośba zgłoszenia bariery
+        msg = "Chcesz zgłosić barierę. Wyślij opis, lokalizację oraz zdjęcia bariery.";
+    } else if(text.indexOf("pomocy") > 0){
+        //użytkownik potrzebuje pomocy
+        msg = "Potrzebujesz pomocy. Szukam wolontariusza w Twojej okolicy.";
+    } else if(text.indexOf("pytanie") > 0){
+        //użytkownik ma pytanie
+        msg = "Masz pytanie. Podaj treść pytania oraz lokalizację, której ono dotyczy.";
+    } else {
+        msg = '[echo] ' + text;
+    }
+    sendMessage(senderId, msg);
+}
 
-function echo(sender, text){
+
+function sendMessage(sender, text){
 	var messageData = {
-		text: '[echo] ' + text
+		text: text
 	};
     request({
         method: 'POST',
