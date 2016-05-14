2ecc47cecfa4d1d15414ef6d44f719d8c871671e
Mateusz Ostafil <ratwolf0@gmail.com>
2016-05-14 13:38:51 +0000

    send sample template message

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ function messageRouter(senderId, text){
     } else if(text.indexOf("pytanie") > 0){
         //użytkownik ma pytanie
         msg = "Masz pytanie. Podaj treść pytania oraz lokalizację, której ono dotyczy.";
+    } else if(text.indexOf("template") > 0){
+        sendTemplate(senderId, text);
     } else {
         msg = '[echo] ' + text;
     }
@@ -86,3 +88,42 @@ function sendMessage(sender, text){
         }
     });
 };
+
+function sendTemplate(sender, text){
+    var messageData = {
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"button",
+            "text":"What do you want to do next?",
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://petersapparel.parseapp.com",
+                "title":"Show Website"
+              },
+              {
+                "type":"postback",
+                "title":"Start Chatting",
+                "payload":"USER_DEFINED_PAYLOAD"
+              }
+            ]
+          }
+        }
+    };
+    request({
+        method: 'POST',
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: secret.token},
+        json: {
+            recipient: {id: sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if(error){
+            console.log('Error sending messages: ', error);
+        } else if(response.body.error){
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
